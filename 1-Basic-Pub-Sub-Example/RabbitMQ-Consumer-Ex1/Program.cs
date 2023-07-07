6ef55932b954ae07c8a7aab280906e5813f1a5be
mert-celik-1 <mertcelik2399@gmail.com>
2023-07-07 22:48:45 +0300

    Add project files.

---
+++ 1-Basic-Pub-Sub-Example/RabbitMQ-Consumer-Ex1/Program.cs
@@ -0,0 +1,33 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+
+
+// Baglantı olusturma
+ConnectionFactory factory = new ConnectionFactory();
+factory.Uri = new("amqps://iqkofwwd:dikwbq0ziS05tUH6lk4etutORFgaZFEI@cow.rmq2.cloudamqp.com/iqkofwwd");
+
+// Baglantıyı aktiflestirme ve kanal açma
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+// Queue oluşturma
+channel.QueueDeclare(queue: "example-queue", exclusive: false);
+
+// Mesaj okuma
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume("example-queue",autoAck: false,consumer);
+
+consumer.Received += (sender, e) =>
+{
+    //Kuyruga gelen mesajın işlendiği yerdir
+    //e.Body : Kuyruktaki mesajın verisini bütünsel olarak getirecektir.
+    //e.Body.Span veya e.Body.ToArray() 
+    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+
+    channel.BasicAck(e.DeliveryTag, false);
+
+};
+
+Console.Read();
---
+++ 1-Basic-Pub-Sub-Example/RabbitMQ-Publisher-Ex1/Program.cs
@@ -0,0 +1,29 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+
+// Baglantı olusturma
+ConnectionFactory factory = new ConnectionFactory();
+factory.Uri = new("amqps://iqkofwwd:dikwbq0ziS05tUH6lk4etutORFgaZFEI@cow.rmq2.cloudamqp.com/iqkofwwd");
+
+// Baglantıyı aktiflestirme ve kanal açma
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+// Queue oluşturma
+channel.QueueDeclare(queue: "example-queue",exclusive:false);
+
+// Queue mesaj gönderme
+
+//RabbitMQ kuyruğa atacağı mesajları byte türünden kabul etmektedir.
+
+//byte[] message = Encoding.UTF8.GetBytes("Merhaba");
+//channel.BasicPublish(exchange:"",routingKey: "example-queue", body: message);
+
+for (int i = 0; i < 100; i++)
+{
+    byte[] message = Encoding.UTF8.GetBytes("Merhaba"+i);
+    channel.BasicPublish(exchange: "", routingKey: "example-queue", body: message);
+}
+
+Console.Read();
